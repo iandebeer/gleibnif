7a43b0526b63a4bb9a91172a376f1d2c47d7f193
ff137 <ff137@proton.me>
2023-08-25 11:04:10 +0200

    refine opportunity title and description

--- modules/client/src/main/scala/dev/mn8/gleibnif/yodawn/GenerateOpportunities.scala
+++ modules/client/src/main/scala/dev/mn8/gleibnif/yodawn/GenerateOpportunities.scala
@@ -7,7 +7,7 @@ object OpportunityGenerator extends App {
   case class Opportunity(
       companyName: String,
       title: String,
-      description: String,
+      description: String = "",
       category: String,
       duration: String,
       location: String,
@@ -21,30 +21,52 @@ object OpportunityGenerator extends App {
     "Goodwall",
     "UNICEF South Africa"
   )
-  val categories = List("Learning", "Task")
-  val languages = List("English", "French", "Spanish")
-  val locations = List("Worldwide", "South Africa", "Nigeria")
+  val categories = List("Learning", "Learning", "Task")
+  val languages = List("English", "English", "English", "French", "Spanish")
+  val locations =
+    List("South Africa", "Nigeria", "Kenya", "Morocco", "Ethiopia")
+
+  val keywords = List(
+    "Programming",
+    "Software Development",
+    "Computing basics",
+    "Beginner Python",
+    "Beginner Java",
+    "Microsoft Office",
+    "Advanced Scala",
+    "Building",
+    "Farming",
+    "Sales",
+    "Leadership",
+    "Economics",
+    "Climate",
+    "Sustainability",
+    "Volunteering"
+  )
 
   def getRandomElement[A](list: List[A]): A = list(Random.nextInt(list.length))
 
-  def getRandomDuration: String = s"${Random.nextInt(100) + 1} Hours"
+  def getRandomDuration: String = s"${Random.nextInt(12) + 1} Hours"
 
   def getRandomTitle: String = Random.nextString(10)
-  def getRandomDescription: String = Random.nextString(10)
 
-  def getRandomOpportunity: Opportunity = Opportunity(
-    companyName = getRandomElement(companies),
-    title = getRandomTitle,
-    description = getRandomDescription,
-    category = getRandomElement(categories),
-    duration = getRandomDuration,
-    location = getRandomElement(locations),
-    language = getRandomElement(languages)
-  )
+  def getDescription(op: Opportunity): String =
+    f"${op.companyName} is offering a ${op.category} opportunity to learn about ${op.title} in ${op.location}"
+
+  def getRandomOpportunity: Opportunity =
+    val op = Opportunity(
+      companyName = getRandomElement(companies),
+      title = getRandomElement(keywords),
+      category = getRandomElement(categories),
+      duration = getRandomDuration,
+      location = getRandomElement(locations),
+      language = getRandomElement(languages)
+    )
+    op.copy(description = getDescription(op))
 
   def generateDataset(numberOfOpportunities: Int): List[Opportunity] =
     (1 to numberOfOpportunities).map(_ => getRandomOpportunity).toList
 
-  val opportunities = generateDataset(10)
+  val opportunities = generateDataset(100)
   opportunities.foreach(println)
 }
